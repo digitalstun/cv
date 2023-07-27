6eee78d80691582d9176803826c28fa4c0eca032
Randy L <49382361+digitalstun@users.noreply.github.com>
2023-07-27 18:03:50 -0400

    added home, contact and redirect routes

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from views import views
 
 app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/views")
 
-@app.route('/')
-def home():
-    return 'Hello World'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- views.py
+++ views.py
@@ -1,1 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for
+
+views = Blueprint(__name__, "views")
+
+# home route
+@views.route("/")
+def home():
+    return render_template("index.html")
+
+# contact route
+@views.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+# redirection route
+@views.route("/go-to-home")
+def go_to_home():
+    return redirect(url_for("views.home"))
